b046eb298bcc35d146cff64ce132faed0d3ddf11
melkenroz <96063713+Melkenroz@users.noreply.github.com>
2024-10-26 20:38:18 +0200

    add eventlistener for site-main-menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let navButton = document.querySelector(".menu-toggler");
+let barTop = document.querySelector(".top");
+let barMiddle = document.querySelector(".middle");
+let barBottom = document.querySelector(".bottom");
+
+navButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // toggle nav state
+  document.body.classList.toggle("menu-visible");
+  barTop.classList.toggle("top-open");
+  barMiddle.classList.toggle("middle-open");
+  barBottom.classList.toggle("bottom-open");
+});
